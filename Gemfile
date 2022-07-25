98fd1f09e63a742371f8ad66be1bc6f9d7de950c
sdfzyanxinyi <1508230297@qq.com>
2022-07-25 21:17:36 +0800

    1
    1

--- Gemfile
+++ Gemfile
@@ -3,5 +3,3 @@ gem 'jekyll-paginate'
 
 gem "jekyll", "~> 4.0"
 gem "rake"
-
-gem "webrick", "~> 1.7"
--- sw.js
+++ sw.js
@@ -19,6 +19,10 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
+  "./img/icon_wechat.png",
+  "./img/avatar-hux.jpg",
+  "./img/home-bg.jpg",
+  "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
@@ -27,6 +31,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
+  "huangxuan.me",
+  "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
